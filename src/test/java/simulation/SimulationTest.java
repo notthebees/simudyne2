4e741df0365fd8aae6b52b6a171362fa614a0045
paddy <paddy@marinus.uk>
2017-09-14 22:18:16 +0100

    Add iteration to test

--- src/test/java/simulation/SimulationTest.java
+++ src/test/java/simulation/SimulationTest.java
@@ -27,20 +27,27 @@ public class SimulationTest {
 
     @Test
     public void appliesUpdaterToAgentsOnEachIteration() throws Exception {
-        final List<Agent> agents = new ArrayList<>();
         Agent agent1 = new Agent(C);
         Agent agent2 = new Agent(NC);
+        Agent updatedAgent1 = new Agent(NC);
+        Agent updatedAgent2 = new Agent(C);
+
+        final List<Agent> agents = new ArrayList<>();
         agents.add(agent1);
         agents.add(agent2);
         Simulation simulation = new Simulation(agents, resultsCalculator, updater);
 
         context.checking(new Expectations() {{
-            oneOf(updater).update(agent1, 1);
-            oneOf(updater).update(agent2, 1);
+            oneOf(updater).update(agent1, 1); will(returnValue(updatedAgent1));
+            oneOf(updater).update(agent2, 1); will(returnValue(updatedAgent2));
+
+            oneOf(updater).update(updatedAgent1, 2);
+            oneOf(updater).update(updatedAgent2, 2);
+
             ignoring(resultsCalculator);
         }});
 
-        simulation.run(1);
+        simulation.run(2);
     }
 
     @Test
