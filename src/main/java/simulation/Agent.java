6427d87250676f7d90c147daf40730a9e6d59762
paddy <paddy@marinus.uk>
2017-09-14 22:35:46 +0100

    Update Agent constructor to include all necessary fields

--- src/main/java/simulation/Agent.java
+++ src/main/java/simulation/Agent.java
@@ -1,17 +1,36 @@
 package simulation;
 
 public class Agent {
-
     public final Breed breed;
-
+    public final int socialGrade;
+    public final int paymentAtPurchase;
+    public final double attributeBrand;
+    public final double attributePrice;
+    public final double attributePromotions;
     public final boolean autoRenew;
+    public final int inertiaForSwitch;
 
-    public Agent(Breed breed, boolean autoRenew) {
+    public Agent(
+            Breed breed,
+            int socialGrade,
+            int paymentAtPurchase,
+            double attributeBrand,
+            double attributePrice,
+            double attributePromotions,
+            boolean autoRenew,
+            int inertiaForSwitch)
+    {
         this.breed = breed;
+        this.socialGrade = socialGrade;
+        this.paymentAtPurchase = paymentAtPurchase;
+        this.attributeBrand = attributeBrand;
+        this.attributePrice = attributePrice;
+        this.attributePromotions = attributePromotions;
         this.autoRenew = autoRenew;
+        this.inertiaForSwitch = inertiaForSwitch;
     }
 
     public Agent(Breed breed) {
-        this(breed, false);
+        this(breed, 0, 0, 0, 0, 0, false, 0);
     }
 }
