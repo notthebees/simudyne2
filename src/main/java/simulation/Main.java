d16e46b05c0d8fe96595409bd9c3bcfa6350b226
paddy <paddy@marinus.uk>
2017-09-16 00:08:24 +0100

    Add main method

---
+++ src/main/java/simulation/Main.java
@@ -0,0 +1,41 @@
+package simulation;
+
+import simulation.agent.Agent;
+import simulation.agent.AgentUpdater;
+import simulation.agent.BreedUpdater;
+import simulation.parsing.DataParser;
+import simulation.random.MockRandom;
+import simulation.results.FunctionalResultsCalculator;
+import simulation.results.Result;
+import simulation.results.ResultsCalculator;
+import simulation.results.SimulationHistory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        File file = new File(Main.class.getResource("/input.csv").getFile());
+        DataParser parser = new DataParser();
+        List<Agent> agents = null;
+        try {
+            agents = parser.parse(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        AgentUpdater updater = new BreedUpdater(new MockRandom());
+        Simulation simulation = new Simulation(agents, updater);
+        SimulationHistory history = simulation.run(15);
+
+        ResultsCalculator resultsCalculator = new FunctionalResultsCalculator();
+        List<Result> results = history.getResults(resultsCalculator);
+        for (int i = 0; i < results.size(); i++) {
+            Result result = results.get(i);
+            System.out.println("Iteration " + Integer.toString(i));
+            System.out.println(result.toString());
+        }
+    }
+}
