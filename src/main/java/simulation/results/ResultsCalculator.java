110ac68776156a385720e0699399a72d4795a2dd
paddy <paddy@marinus.uk>
2017-09-15 21:39:46 +0100

    Fix git fiasco

--- src/main/java/simulation/results/ResultsCalculator.java
+++ src/main/java/simulation/results/ResultsCalculator.java
@@ -2,27 +2,11 @@ package simulation.results;
 
 import simulation.agent.Agent;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static simulation.agent.Breed.C;
+public interface ResultsCalculator {
 
-public class FunctionalResultsCalculator implements ResultsCalculator {
+    Result calculateResults(List<Agent> agents);
 
-    @Override
-    public Result calculateResults(List<Agent> agents) {
-        int breedCAgents = (int) agents.stream()
-                .filter(agent -> agent.breed.equals(C))
-                .count();
-        int breedNCAgents = agents.size() - breedCAgents;
-
-        return new Result(breedCAgents, breedNCAgents, 0, 0, 0);
-    }
-
-    @Override
-    public List<Result> calculateResults(SimulationHistory history) {
-        List<Result> results = new ArrayList<>();
-        results.add(calculateResults(history.get(0)));
-        return results;
-    }
+    List<Result> calculateResults(SimulationHistory history);
 }
