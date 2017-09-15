8c90f94542348e101fe0f4d7d38d674a8ffe0322
paddy <paddy@marinus.uk>
2017-09-15 21:16:26 +0100

    Implement SimulationHistory

--- src/main/java/simulation/results/SimulationHistory.java
+++ src/main/java/simulation/results/SimulationHistory.java
@@ -1,4 +1,19 @@
 package simulation.results;
 
+import simulation.agent.Agent;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SimulationHistory {
+
+    private final List<List<Agent>> history = new ArrayList<>();
+
+    public void add(List<Agent> agents) {
+        history.add(agents);
+    }
+
+    public List<Agent> get(int i) {
+        return history.get(i);
+    }
 }
